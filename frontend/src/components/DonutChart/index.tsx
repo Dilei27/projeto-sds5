2cb76a3cf27f760bacce3e240c35dca42c814f95
Dilei <dileipaula@gmail.com>
2021-11-16 11:51:19 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -5,42 +5,60 @@ import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
-    labels: string[];
-    series: number[];
+@@ -10, 26 + 11, 25 @@type ChartData = {
 
-}
+    const DonutChart = () => {
 
-const DonutChart = () => {
+        let chartData: ChartData = { labels: [], series: [] };
+        const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
-
-    useEffect(() => {
-        axios.get(BASE_URL + '/sales/amount-by-seller')
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
-                const data = response.data as SaleSum[];
-                const myLabels = data.map(x => x.sellerName);
-                const mySeries = data.map(x => x.sum);
+                useEffect(() => {
+                    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+                        .then(response => {
 
-                setChartData({ labels: myLabels, series: mySeries });
-            });
-    }, []);
+                            const data = response.data as SaleSum[];
+                            const myLabels = data.map(x => x.sellerName);
+                            const mySeries = data.map(x => x.soma);
+                            const data = response.data as SaleSum[];
+                            const myLabels = data.map(x => x.sellerName);
+                            const mySeries = data.map(x => x.soma);
 
-    const options = {
-        legend: {
-            show: true
-        }
-    }
+                            chartData = {
+                                labels: myLabels,
+                                series: mySeries
+                            }
+                            console.log(response.data);
+                        });
 
-    return (
-        <Chart
-            options={{ ...options, labels: chartData.labels }}
-            series={chartData.series}
-            type="donut"
-            heigth="240"
+                    const mockData = {
+                        series: [477138, 499928, 444867, 220426],
+                        labels: ['Calebe', 'Luanda', 'Santiago', 'Valadares']
+                    };
+                    setChartData({ labels: myLabels, series: mySeries });
+                    console.log(response.data);
+                });
+            }, []);
 
-        />
+        //    const mockData = {
+        //        series: [477138, 499928, 444867, 220426],
+        //        labels: ['Calebe', 'Luanda', 'Santiago', 'Valadares']
+        //    };
 
-    );
-}
+        const options = {
+            legend: {
+                show: true
+            }
+        };
 
-export default DonutChart;
+        return (
+            <Chart
+                options={{ ...options, labels: chartData.labels }}
+                series={chartData.series}
+                type="donut"
+                height="240"
+            />
+        );
+    };
+    export default DonutChart;
